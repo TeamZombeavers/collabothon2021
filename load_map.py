2e20ac62d18ac5c7d785f3ee3b4c41d63dfe7b34
Adrian P <ad.pajka@gmail.com>
2021-07-07 10:06:20 -0700

    added radius for visualization

--- load_map.py
+++ load_map.py
@@ -3,20 +3,24 @@ from bokeh.plotting import gmap
 from bokeh.io import show
 import pandas as pd
 
+from bokeh.transform import linear_cmap
+from bokeh.palettes import Plasma256 as palette
 
 api_key = 'AIzaSyCCKJAMQRcfWjlXJMQhzsVA22FbAGqEDZM'
 center_map_lat, center_map_lng = (51.834070, 19.355114) # center point for starting map TODO: change to first values from table
 
 trees_df = pd.read_csv('drzewa.csv')
 
-def load_google_map(lat, lng, zoom=10, map_type='roadmap'):
+def load_google_map(lat, lng, zoom=17, map_type='roadmap'):
     gmap_options = GMapOptions(lat=lat, lng=lng,
                                map_type=map_type, zoom=zoom)
     gmap_obj = gmap(api_key, gmap_options, title='Trees density',
              width=550, height=450)
     source = ColumnDataSource(trees_df)
 
-    center = gmap_obj.circle('X', 'Y', size=4, alpha=0.5, color='green', source=source)          
+    mapper = linear_cmap('Height', palette, 2000.,8000.)
+
+    center = gmap_obj.circle('X', 'Y', size=4, alpha=0.5, color=mapper, radius='Radius', source=source)          
     show(gmap_obj)
     return gmap_obj
 
