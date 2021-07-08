461fb07d0db285e310494f9b6b680d826969c2e9
Adrian P <ad.pajka@gmail.com>
2021-07-08 00:11:31 -0700

    different colours, hover info feature, bar legend

--- load_map.py
+++ load_map.py
@@ -1,4 +1,4 @@
-from bokeh.models import ColumnDataSource, GMapOptions
+from bokeh.models import ColumnDataSource, GMapOptions, HoverTool, ColorBar
 from bokeh.plotting import gmap
 from bokeh.io import show
 import pandas as pd
@@ -6,21 +6,39 @@ import pandas as pd
 from bokeh.transform import linear_cmap
 from bokeh.palettes import Plasma256 as palette
 
+
 api_key = 'AIzaSyCCKJAMQRcfWjlXJMQhzsVA22FbAGqEDZM'
-center_map_lat, center_map_lng = (51.834070, 19.355114) # center point for starting map TODO: change to first values from table
+# center point for starting map TODO: change to first values from table
+center_map_lat, center_map_lng = (51.834070, 19.355114)
 
 trees_df = pd.read_csv('drzewa.csv')
+
 
 def load_google_map(lat, lng, zoom=17, map_type='roadmap'):
     gmap_options = GMapOptions(lat=lat, lng=lng,
                                map_type=map_type, zoom=zoom)
+
+    hover = HoverTool(
+        tooltips=[
+
+            ('height of tree [m]', '@Height'),
+            ('upper branches of a tree width [m]', '@Radius'),
+
+        ]
+    )
+
     gmap_obj = gmap(api_key, gmap_options, title='Trees density',
-             width=550, height=450)
+                    width=550, height=450, tools=[hover, 'reset', 'wheel_zoom', 'pan'])
     source = ColumnDataSource(trees_df)
 
-    mapper = linear_cmap('Height', palette, 2000.,8000.)
+    mapper = linear_cmap('Height', palette, 1., 40.)
 
-    center = gmap_obj.circle('X', 'Y', size=4, alpha=0.5, color=mapper, radius='Radius', source=source)          
+    center = gmap_obj.circle('X', 'Y', alpha=0.5,
+                             color=mapper, radius='Radius', source=source)
+
+    color_bar = ColorBar(color_mapper=mapper['transform'], label_standoff=14,
+                         location=(0, 0), title="Tree height [m]")
+    gmap_obj.add_layout(color_bar, 'right')
     show(gmap_obj)
     return gmap_obj
 
